700abe520d467fe44ac5d18e7b28efab12886b69
Yassine <yassine@indexifi.io>
2022-12-21 12:45:35 +0000

    Connect DApp to localhost through MetaMask

--- webapp/src/pages/index.tsx
+++ webapp/src/pages/index.tsx
@@ -5,7 +5,65 @@ import NextLink from "next/link"
 import { VStack, Heading, Box, LinkOverlay, LinkBox} from "@chakra-ui/layout"
 import { Text, Button } from '@chakra-ui/react'
 
+import { useState, useEffect} from 'react'
+import {ethers} from "ethers"
+
+declare let window:any
+
 const Home: NextPage = () => {
+  const [balance, setBalance] = useState<string | undefined>()
+  const [currentAccount, setCurrentAccount] = useState<string | undefined>()
+  const [chainId, setChainId] = useState<number | undefined>()
+  const [chainname, setChainName] = useState<string | undefined>()
+
+  useEffect(() => {
+    if(!currentAccount || !ethers.utils.isAddress(currentAccount)) return
+    //client side code
+    if(!window.ethereum) return
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    provider.getBalance(currentAccount).then((result)=>{
+      setBalance(ethers.utils.formatEther(result))
+    })
+    provider.getNetwork().then((result)=>{
+      setChainId(result.chainId)
+      setChainName(result.name)
+    })
+
+  },[currentAccount])
+
+  const onClickConnect = () => {
+    //client side code
+    if(!window.ethereum) {
+      console.log("please install MetaMask")
+      return
+    }
+    /*
+    //change from window.ethereum.enable() which is deprecated
+    //see docs: https://docs.metamask.io/guide/ethereum-provider.html#legacy-methods
+    window.ethereum.request({ method: 'eth_requestAccounts' })
+    .then((accounts:any)=>{
+      if(accounts.length>0) setCurrentAccount(accounts[0])
+    })
+    .catch('error',console.error)
+    */
+
+    //we can do it using ethers.js
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+
+    // MetaMask requires requesting permission to connect users accounts
+    provider.send("eth_requestAccounts", [])
+    .then((accounts)=>{
+      if(accounts.length>0) setCurrentAccount(accounts[0])
+    })
+    .catch((e)=>console.log(e))
+  }
+
+  const onClickDisconnect = () => {
+    console.log("onClickDisConnect")
+    setBalance(undefined)
+    setCurrentAccount(undefined)
+  }
+
   return (
     <>
       <Head>
@@ -14,24 +72,25 @@ const Home: NextPage = () => {
 
       <Heading as="h3"  my={4}>Explore Web3</Heading>          
       <VStack>
-        <Box  my={4} p={4} w='100%' borderWidth="1px" borderRadius="lg">
-          <Heading my={4}  fontSize='xl'>Task 1</Heading>
-          <Text>local chain with hardhat</Text>
+        <Box w='100%' my={4}>
+        {currentAccount  
+          ? <Button type="button" w='100%' onClick={onClickDisconnect}>
+                Account:{currentAccount}
+            </Button>
+          : <Button type="button" w='100%' onClick={onClickConnect}>
+                  Connect MetaMask
+              </Button>
+        }
         </Box>
-
-        <Box  my={4} p={4} w='100%' borderWidth="1px" borderRadius="lg">
-          <Heading my={4}  fontSize='xl'>Task 2</Heading>
-          <Text>DAPP with React/NextJS/Chakra</Text>
+        {currentAccount  
+          ?<Box  mb={0} p={4} w='100%' borderWidth="1px" borderRadius="lg">
+          <Heading my={4}  fontSize='xl'>Account info</Heading>
+          <Text>ETH Balance of current account: {balance}</Text>
+          <Text>Chain Info: ChainId {chainId} name {chainname}</Text>
         </Box>
-
-        <LinkBox  my={4} p={4} w='100%' borderWidth="1px" borderRadius="lg">
-          <NextLink href="https://github.com/NoahZinsmeister/web3-react/tree/v6" passHref>
-          <LinkOverlay>
-            <Heading my={4} fontSize='xl'>Task 3 with link</Heading>
-            <Text>Read docs of Web3-React V6</Text>
-          </LinkOverlay>
-          </NextLink>
-        </LinkBox>
+        :<></>
+        }
+...
       </VStack>
     </>
   )
